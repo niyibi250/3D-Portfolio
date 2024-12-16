15438fc7b415ea4b21beb017cac3c13b1babdf82
niyibi250 <ericniyibizi1998@gmail.com>
2024-12-16 14:57:59 +0200

    fix: the asset import

---
+++ src/assets/index.d.ts
@@ -0,0 +1,51 @@
+import mobile from "./mobile.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import web from "./web.png";
+import javascript from "./javascript.png";
+import typescript from "./typescript.png";
+import html from "./html.png";
+import css from "./css.png";
+import reactjs from "./reactjs.png";
+import redux from "./redux.png";
+import tailwind from "./tailwind.png";
+import nodejs from "./nodejs.png";
+import mongodb from "./mongodb.png";
+import git from "./git.png";
+import figma from "./figma.png";
+import docker from "./docker.png";
+import meta from "./meta.png";
+import starbucks from "./starbucks.png";
+import tesla from "./tesla.png";
+import shopify from "./shopify.png";
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import threejs from "./threejs.png";
+
+export {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+};
