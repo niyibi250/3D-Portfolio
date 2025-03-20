01e79338dc70ebae0cab869eee539d333cf20405
niyibi250 <ericniyibizi1998@gmail.com>
2025-03-20 15:39:40 +0200

    change testonial

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -145,7 +145,7 @@ import {
   const testimonials = [
     {
       testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+        "I thought it was impossible to make a website as beautiful as our product, but Eric proved me wrong.",
       name: "Sara Lee",
       designation: "CFO",
       company: "Acme Co",
@@ -153,7 +153,7 @@ import {
     },
     {
       testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+        "I've never met a web developer who truly cares about their clients' success like Eric does.",
       name: "Chris Brown",
       designation: "COO",
       company: "DEF Corp",
@@ -161,7 +161,7 @@ import {
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+        "After Eric optimized our website, our traffic increased by 50%. We can't thank them enough!",
       name: "Lisa Wang",
       designation: "CTO",
       company: "456 Enterprises",
